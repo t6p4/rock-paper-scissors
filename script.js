2516d8e3be03b72b781d84ff4c4ba06b0c639b95
t6p4 <155362069+t6p4@users.noreply.github.com>
2024-01-04 08:37:01 +0200

    Create playRound function

--- script.js
+++ script.js
@@ -4,3 +4,16 @@ function getComputerChoice() {
     let choice = Math.floor(Math.random() * choices.length)
     return choices[choice]
 }
+
+function playRound() {
+    let userChoice = prompt("Choose your weapon:");
+    userChoice = userChoice.charAt(0).toUpperCase() + userChoice.slice(1);
+    let computerChoice = getComputerChoice()
+    if (userChoice === computerChoice) {
+        console.log("It's a tie!")
+    } else if (userChoice === "Rock" && computerChoice === "Scissors" || userChoice === "Paper" && computerChoice === "Rock" || userChoice === "Scissors" && computerChoice === "Paper") {
+        console.log("You win!")
+    } else {
+        console.log("You lose!")
+    }
+}
