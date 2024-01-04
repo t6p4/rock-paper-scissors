8aea8d7f6f6cc6551c396ef6cfef8dc577bd3419
t6p4 <155362069+t6p4@users.noreply.github.com>
2024-01-04 09:27:54 +0200

    Edit script to work on UI

--- script.js
+++ script.js
@@ -2,24 +2,52 @@ const choices = ["Rock", "Paper", "Scissors"]
 let userScore = 0;
 let computerScore = 0;
 
+const buttons = document.querySelectorAll("button");
+const roundResult = document.querySelector(".roundResult")
+const finalResult = document.querySelector(".finalResult")
+
+buttons.forEach((button) => {
+    button.addEventListener("click", (e) => {
+        
+        playRound(e.target.textContent)
+    })
+})
+
 function getComputerChoice() {
     let choice = Math.floor(Math.random() * choices.length)
     return choices[choice]
 }
 
-function playRound() {
-    let userChoice = prompt("Choose your weapon:");
+function playRound(userChoice) {
+    if (finalResult) {
+        finalResult.textContent = ""
+    }
     userChoice = userChoice.charAt(0).toUpperCase() + userChoice.slice(1);
     let computerChoice = getComputerChoice()
     if (userChoice === computerChoice) {
-        console.log("It's a tie!")
+        roundResult.textContent = "It's a tie!"
     } else if (userChoice === "Rock" && computerChoice === "Scissors" || userChoice === "Paper" && computerChoice === "Rock" || userChoice === "Scissors" && computerChoice === "Paper") {
-        console.log("You win!")
+        roundResult.textContent = "You win!"
         userScore++;
     } else {
-        console.log("You lose!")
+        roundResult.textContent = "You lose!"
         computerScore++;
     }
+    if (userScore == 5 || computerScore == 5) {
+        if (userScore > computerScore) {
+        finalResult.textContent = `You are victorious! Your score is ${userScore}, while your opponent has ${computerScore} points`
+        userScore = 0;
+        computerScore = 0;
+    } else if (computerScore > userScore) {
+        finalResult.textContent = `You lost! Your score is ${userScore}, while your opponent has ${computerScore} points`
+        userScore = 0;
+        computerScore = 0;
+    } else {
+        finalResult.textContent = `It's a tie! You both have ${userScore} points!`
+        userScore = 0;
+        computerScore = 0;
+    }
+}
 }
 
 function game() {
