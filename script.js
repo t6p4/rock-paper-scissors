f62b06e94e6cc1dc5bb78815111ec9a398dc13cc
t6p4 <155362069+t6p4@users.noreply.github.com>
2024-01-04 08:43:22 +0200

    Create game loop function with scoring

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 const choices = ["Rock", "Paper", "Scissors"]
+let userScore = 0;
+let computerScore = 0;
 
 function getComputerChoice() {
     let choice = Math.floor(Math.random() * choices.length)
@@ -13,7 +15,22 @@ function playRound() {
         console.log("It's a tie!")
     } else if (userChoice === "Rock" && computerChoice === "Scissors" || userChoice === "Paper" && computerChoice === "Rock" || userChoice === "Scissors" && computerChoice === "Paper") {
         console.log("You win!")
+        userScore++;
     } else {
         console.log("You lose!")
+        computerScore++;
+    }
+}
+
+function game() {
+    for(let i = 0; i < 5; i++) {
+        playRound()
+    } 
+    if (userScore > computerScore) {
+        console.log(`You are victorious! Your score is ${userScore}, while your opponent has ${computerScore} points`)
+    } else if (computerScore > userScore) {
+        console.log(`You lost! Your score is ${userScore}, while your opponent has ${computerScore} points`)
+    } else {
+        console.log(`It's a tie! You both have ${userScore} points!`)
     }
 }
